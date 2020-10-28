93640d8ff7ee801c898e742a38708f825e70b081
souravb1612 <63535731+souravb1612@users.noreply.github.com>
2020-10-28 20:17:40 +0530

    Update correctSVMprogram.py

--- correctSVMprogram.py
+++ correctSVMprogram.py
@@ -1,3 +1,13 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import pandas as pd
+df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/00476/buddymove_holidayiq.csv')
+print(df)
+df.shape
+features=['Picnic','Religious','Nature','Theatre','Shopping']
+X=df[features]
+Y=df.Sports
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.30)
 from sklearn import datasets
 from sklearn.metrics import accuracy_score
 from sklearn import svm
